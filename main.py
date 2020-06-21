c63b07b964500801332114f2fbc86d2df7d63f27
Jeevan Rupacha <rupachasgvn@gmail.com>
2020-06-21 13:58:57 +0545

    version 2.0.0 upgrade --change backend python class based threading implementation

--- main.py
+++ main.py
@@ -1,117 +1,42 @@
+from python import MyMain
+from flask import Flask, render_template,request
 import requests
-from bs4 import BeautifulSoup
-from flask import Flask, request,render_template,Markup
+import time
+import threading 
 
-app = Flask(__name__, static_url_path='/static')
-selectedLink = []
-selectedTitle = []
-searchResultDataLists = []
+app = Flask(__name__)
 
-
-
+m = MyMain.MyMain()
+#headers for requests the url 
 @app.route('/')
 def render_html():
-    return render_template('./index.html',html=searchResultDataLists,length=len(selectedLink),selectedLink=selectedLink,selectedTitle=selectedTitle)
+    return render_template('./index.html',html=m.searchResultDataLists,length=len(m.selectedLink),selectedLink=m.selectedLink,selectedTitle=m.selectedTitle)
 
 
 #on submit the form router
 @app.route('/submit_form', methods=['POST', 'GET'])
 def getFormValues():
+    start = time.perf_counter()
     if request.method == 'POST':
         data = request.form.to_dict()
         search = data['search-text']
-        searchFun(search)
-        for i in range(len(selectedLink)):
-            scrabEachWebsite(selectedLink[i],selectedTitle[i])
+        m.searchFun(search)
+        threads = []
+        for i in range(len(m.selectedLink)):
+            t = threading.Thread(target=m.scrabEachWebsite, args=[m.selectedLink[i],m.selectedTitle[i]])
+            t.start()
+            threads.append(t)
 
-        return render_template('./index.html',submitform ="true",html=searchResultDataLists,length=len(selectedLink),selectedLink=selectedLink,selectedTitle=selectedTitle)
+        for thread in threads:
+            thread.join()
+       
+        finished = time.perf_counter()
+
+
+        print( finished - start )
+        return render_template('./index.html',submitform ="true",html=m.searchResultDataLists,length=len(m.selectedLink),selectedLink=m.selectedLink,selectedTitle=m.selectedTitle)
     else:
         return "submiting from error"
-
-
-
-#searching algorithm       
-def searchFun(search):
-    selectedLink.clear()   
-    selectedTitle.clear()
-    searchResultDataLists.clear()   
-    response = requests.get("https://www.google.com/search?q=" + search)
-    print(response.url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    #google result kCrYT class contains the links tags 
-    #selecting <a> tag inside all the kCrYT class
-    anker = soup.select('.kCrYT a')
-    #looping all the anker tag for links and head extract
-    for i in range(len(anker)):
-        h3 = anker[i].find('h3')
-        if h3 != None:
-            #append the link href to selectedLink list
-            print(anker[i])
-            #text.split("&",1)[0] replace all the string after the '&' character
-            aLink = anker[i].get('href').replace('/url?q=',"").split('&',1)[0]
-            selectedLink.append(aLink.split('%',1)[0])
-            selectedTitle.append(h3.get_text())
-    display()    
-
-#calcuation for each linked website function 
-def scrabEachWebsite(url,title):
-    if "youtube.com" not in url:
-        responseweb = requests.get(url)
-        soupweb = BeautifulSoup(responseweb.text, 'lxml')
-        webDataExtract(soupweb)
-
-
-#Extract the web data
-#this is algorithm for filtering the web data
-def webDataExtract(soup):
-    #check article tag
-    if (soup.find('article')):
-        child = soup.find('article').find_all('div')
-        return articleExtractor(child)
-
-    #content id tag
-    if (soup.find(id={'content'})):
-        child = soup.find(id={'content'}).find_all('div')
-        return articleExtractor(child)
-
-    #content class tag
-    if (soup.find(class_={'content'})):
-        child = soup.find(class_={'content'}).find_all('div')
-        return articleExtractor(child)
-
-    #content-main id tag
-    if (soup.find(id={'content-main'})):
-        child = soup.find(id={'content-main'}).find_all('div')
-        return articleExtractor(child)
-
-    #content-main class tag
-    if (soup.find(class_={'content-main'})):
-        child = soup.find(class_={'content-main'}).find_all('div')
-        return articleExtractor(child)
-
-    #content-main class tag
-    if (soup.find(class_={'article-container'})):
-        child = soup.find(class_={'content-main'}).find_all('div')
-        return articleExtractor(child)
-
-
-
-# article extracting function *only by article tag
-#Extracts the all children od article tag i.e div tag and take largest length div
-def articleExtractor(child):
-    big = ""
-    for div in child:
-        if(len(big)< len(div)):
-            big = div
-    searchResultDataLists.append(Markup(big))
-
-
-
-def display():
-    for i in range(len(selectedTitle)):
-        print(selectedLink[i])
-        print(selectedTitle[i] + "\n\n")
- 
 
 
 if __name__ == '__main__':
