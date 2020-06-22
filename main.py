ad505a2a88aa6979caa54bbf57021c9e34ff5987
Jeevan Rupacha <rupachasgvn@gmail.com>
2020-06-22 23:50:30 +0545

    Fixed the multithreading --loads fast

--- main.py
+++ main.py
@@ -1,73 +1,54 @@
-from PythonBack import MyMain
+from PythonBack import Searching
 from flask import Flask, render_template,request
 import requests
 import time
-import threading
-from concurrent.futures import ProcessPoolExecutor, as_completed
-import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+
 
 app = Flask(__name__)
 
-start = time.perf_counter()
 
-m = MyMain.MyMain()
+start = time.perf_counter()
+searchClass = Searching.SearchClass()
 #headers for requests the url 
 @app.route('/')
 def render_html():
-    return render_template('./index.html',html=m.searchResultDataLists,length=len(m.selectedLink),selectedLink=m.selectedLink,selectedTitle=m.selectedTitle)
-
-
+    return render_template('./index.html',html=searchClass.searchResultDataLists,length=len(searchClass.selectedLink),selectedLink=searchClass.selectedLink,selectedTitle=searchClass.selectedTitle)
 #on submit the form router
 @app.route('/submit_form', methods=['POST', 'GET'])
 def getFormValues():
     if request.method == 'POST':
         data = request.form.to_dict()
         search = data['search-text']
-        m.searchFun(search)
-
-        # try:
-        #     with ProcessPoolExecutor(max_workers=4) as executor:
-        #         start = time.time()
-        #         futures = [executor.submit(m.scrabEachWebsite, url) for url in m.selectedLink]
-        #         results = []
-        #         for result in as_completed(futures):
-        #             results.append(result)
-        #         end = time.time()
-        #         print("Time Taken: {:.6f}s".format(end - start))
-        # except:
-        #     print(EOFError)
-
+        startSearch = time.perf_counter()
+        searchClass.searchFun(search)
+        endSearch = time.perf_counter()
+        print(endSearch - startSearch)
 
 
         # try:
-        #     with concurrent.futures.ThreadPoolExecutor() as executor:
-        #         executor.map( m.scrabEachWebsite,m.selectedLink)
+        #     with ProcessPoolExecutor(max_workers = 20) as executor:
+        #         futures = [executor.submit(searchClass.scrabEachWebsite, url) for url in searchClass.selectedLink]
+               
+        # except Exception as e: print(e)
 
-        # except:
-        #     raise(Exception)
-        # process = []
-        # try:
-        #     for i in range(len(m.selectedLink)):
-        #         p = multiprocessing.Process(target=m.scrabEachWebsite, args=[m.selectedLink[i]])
-        #         p.start()
-        #         process.append(p)
 
-        #     for pro in process:
-        #         pro.join()
-        # except:
-        #     print("error herer")
-        for i in range(len(m.selectedLink)):
-            m.scrabEachWebsite(m.selectedLink[i])
-         
-        finished = time.perf_counter()
-        print( finished - start )
-        return render_template('./index.html',submitform ="true",html=m.searchResultDataLists,length=len(m.selectedLink),selectedLink=m.selectedLink,selectedTitle=m.selectedTitle)
+        try:
+            with ThreadPoolExecutor() as executor:
+                resutls = executor.map( searchClass.scrabEachWebsite,searchClass.selectedLink)
+            finished = time.perf_counter()
+            print("parsing data" + (finished - start) )
+        except Exception as e: print(e)
+
+
+        return render_template('./index.html',submitform ="true",html=searchClass.searchResultDataLists,length=len(searchClass.selectedLink),selectedLink=searchClass.selectedLink,selectedTitle=searchClass.selectedTitle)
     else:
         return "submiting from error"
 
 
-
-if __name__ == '__main__': 
+if __name__ == '__main__':
     app.debug = True
     app.run()
-    app.run(debug = True)
+    app.run(debug=True)
+    
